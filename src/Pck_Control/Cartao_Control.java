8735cc4bcceebd2449c5519ad5766115e81b3066
dti <dti@LAB204-26>
2023-10-25 10:00:20 -0300

    Adicionando todas as classes Control e Persistencia

--- src/Pck_Control/Cartao_Control.java
+++ src/Pck_Control/Cartao_Control.java
@@ -1,7 +1,46 @@
 package Pck_Control;
 
 import Pck_Model.Cartao_Model;
+import Pck_Persistencia.Cartao_Persistencia;
 
 public class Cartao_Control {
+
+	Cartao_Model Cartao_Model = new Cartao_Model();
+	Cartao_Persistencia Cartao_Persistencia = new Cartao_Persistencia();
+
+	public void PROC_INSERIR_CARTAO_04(int iIdCartao, String sTitularCartao, String sValidadeCartao, String sNumeroCartao, int iCvcCartao, int iIdPlano) {
+
+		Cartao_Model.setA04_idcartao(iIdCartao);
+		Cartao_Model.setA04_titularcartao(sTitularCartao);
+		Cartao_Model.setA04_validadecartao(sValidadeCartao);
+		Cartao_Model.setA04_numerocartao(sNumeroCartao);
+		Cartao_Model.setA04_cvccartao(iCvcCartao);
+		Cartao_Model.setA03_idplano(iIdPlano);
+		
+		//Bounty_Persistencia.PROC_INSERIR_CLIENTE(Bounty_Model);
+		Cartao_Persistencia.PROC_INSERIR_CARTAO_04(Cartao_Model);
+	}
+
+	public void PROC_ATUALIZAR_CARTAO_04(int iIdCartao, String sTitularCartao, String sValidadeCartao, String sNumeroCartao, int iCvcCartao, int iIdPlano) {
+		
+		Cartao_Model.setA04_idcartao(iIdCartao);
+		Cartao_Model.setA04_titularcartao(sTitularCartao);
+		Cartao_Model.setA04_validadecartao(sValidadeCartao);
+		Cartao_Model.setA04_numerocartao(sNumeroCartao);
+		Cartao_Model.setA04_cvccartao(iCvcCartao);
+		Cartao_Model.setA03_idplano(iIdPlano);
+		
+		//Bounty_Persistencia.PROC_INSERIR_CLIENTE(Bounty_Model);
+		Cartao_Persistencia.PROC_ATUALIZAR_CARTAO_04(Cartao_Model);
+		
+	}
+
+	public void PROC_EXCLUIR_CARTAO_04(int iIdCartao) {
+		
+		Cartao_Model.setA04_idcartao(iIdCartao);
+		
+		Cartao_Persistencia.PROC_EXCLUIR_CARTAO_04(Cartao_Model);
+		
+	}
 
 }
--- src/Pck_Persistencia/Cartao_Persistencia.java
+++ src/Pck_Persistencia/Cartao_Persistencia.java
@@ -6,11 +6,69 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Pck_DAO.Conexao_MySQL;
+import Pck_Model.Cartao_Model;
 
 public class Cartao_Persistencia {
 	CallableStatement oCall;
 	Conexao_MySQL oConectar = new Conexao_MySQL();
 	PreparedStatement oPrepared;
 	ResultSet oResultSet;
+
+	public void PROC_INSERIR_CARTAO_04(Cartao_Model Cartao_Model) {
+		try {
+			try {
+
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_INSERIR_CARTAO_04(?, ?, ?, ?, ?, ?, ?, ?)");
+				oCall.setInt(1, Cartao_Model.getA04_idcartao());
+				oCall.setString(2, Cartao_Model.getA04_titularcartao());
+				oCall.setString(3, Cartao_Model.getA04_validadecartao());
+				oCall.setString(4, Cartao_Model.getA04_numerocartao());
+				oCall.setInt(5, Cartao_Model.getA04_cvccartao());
+				oCall.setInt(6, Cartao_Model.getA03_idplano());
+
+				oCall.execute();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public void PROC_ATUALIZAR_CARTAO_04(Cartao_Model Cartao_Model) {
+		try {
+			try {
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_ATUALIZAR_CARTAO_04(?, ?, ?, ?, ?, ?, ?, ?)");
+				oCall.setInt(1, Cartao_Model.getA04_idcartao());
+				oCall.setString(2, Cartao_Model.getA04_titularcartao());
+				oCall.setString(3, Cartao_Model.getA04_validadecartao());
+				oCall.setString(4, Cartao_Model.getA04_numerocartao());
+				oCall.setInt(5, Cartao_Model.getA04_cvccartao());
+				oCall.setInt(6, Cartao_Model.getA03_idplano());
+
+				oCall.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public void PROC_EXCLUIR_CARTAO_04(Cartao_Model Cartao_Model) {
+		try {
+			try {
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_EXCLUIR_CARTAO_04(?)");
+				oCall.setInt(1, Cartao_Model.getA04_idcartao());
+
+				oCall.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 
 }
