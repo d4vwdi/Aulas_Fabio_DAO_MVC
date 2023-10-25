ef9b567d7bf20c75f00d94171f35dd3dff39cdfb
dti <dti@LAB204-26>
2023-10-25 09:04:22 -0300

    Adicionando todas as classes de Plano

--- src/Pck_Control/Plano_Control.java
+++ src/Pck_Control/Plano_Control.java
@@ -1,7 +1,37 @@
 package Pck_Control;
 
 import Pck_Model.Plano_Model;
+import Pck_Persistencia.Plano_Persistencia;
 
 public class Plano_Control {
 
+	Plano_Model Plano_Model = new Plano_Model();
+	Plano_Persistencia Plano_Persistencia = new Plano_Persistencia();
+	
+	public void PROC_INSERIR_PLANO_03(int iIdPlano, String sNomePlano, String sPorcentagemCashbackPlano, String sParceriasPlano) {
+		Plano_Model.setA03_idplano(iIdPlano);
+		Plano_Model.setA03_nomeplano(sNomePlano);
+		Plano_Model.setA03_parceriasplano(sParceriasPlano);
+		Plano_Model.setA03_porcentagemcashbackplano(sPorcentagemCashbackPlano);
+		
+		Plano_Persistencia.PROC_INSERIR_PLANO_03(Plano_Model);
+	}
+	
+	public void PROC_ATUALIZAR_PLANO_03(int iIdPlano, String sNomePlano, String sPorcentagemCashbackPlano, String sParceriasPlano) {
+		Plano_Model.setA03_idplano(iIdPlano);
+		Plano_Model.setA03_nomeplano(sNomePlano);
+		Plano_Model.setA03_parceriasplano(sParceriasPlano);
+		Plano_Model.setA03_porcentagemcashbackplano(sPorcentagemCashbackPlano);
+		
+		Plano_Persistencia.PROC_ATUALIZAR_PLANO_03(Plano_Model);
+	}
+	
+	public void PROC_EXCLUIR_PLANO_03(int iIdPlano, String sNomePlano, String sPorcentagemCashbackPlano, String sParceriasPlano) {
+		Plano_Model.setA03_idplano(iIdPlano);
+		
+		Plano_Persistencia.PROC_EXCLUIR_PLANO_03(Plano_Model);
+	}
+	
+	
+	
 }
--- src/Pck_Persistencia/Plano_Persistencia.java
+++ src/Pck_Persistencia/Plano_Persistencia.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Pck_DAO.Conexao_MySQL;
+import Pck_Model.Plano_Model;
 
 public class Plano_Persistencia {
 	CallableStatement oCall;
@@ -13,5 +14,59 @@ public class Plano_Persistencia {
 	PreparedStatement oPrepared;
 	ResultSet oResultSet;
 	
+	public void PROC_INSERIR_PLANO_03(Plano_Model Plano_Model) {
+		try {
+			try {
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_INSERIR_PLANO_03(?, ?, ?, ?)");
+				oCall.setInt(1, Plano_Model.getA03_idplano());
+				oCall.setString(2, Plano_Model.getA03_nomeplano());
+				oCall.setString(3, Plano_Model.getA03_porcentagemcashbackplano());
+				oCall.setString(4, Plano_Model.getA03_parceriasplano());
+
+
+				oCall.execute();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 	
+	public void PROC_ATUALIZAR_PLANO_03(Plano_Model Plano_Model) {
+		try {
+			try {
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_ATUALIZAR_PLANO_03(?, ?, ?, ?)");
+				oCall.setInt(1, Plano_Model.getA03_idplano());
+				oCall.setString(2, Plano_Model.getA03_nomeplano());
+				oCall.setString(3, Plano_Model.getA03_porcentagemcashbackplano());
+				oCall.setString(4, Plano_Model.getA03_parceriasplano());
+
+
+				oCall.execute();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public void PROC_EXCLUIR_PLANO_03(Plano_Model Plano_Model) {
+		try {
+			try {
+				oCall = oConectar.getConexao().prepareCall("CALL PROC_EXCLUIR_PLANO_03(?)");
+				oCall.setInt(1, Plano_Model.getA03_idplano());
+				
+				oCall.execute();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
